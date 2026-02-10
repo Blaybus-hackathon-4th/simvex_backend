5f8c8c19a16b0f547c12c1a82e08536c22811c02
kimyeongeuk <tklr0731@naver.com>
2026-02-10 12:08:40 +0900

    refactor: cors 추가

--- src/main/java/com/example/blaybus4th/global/config/SecurityConfig.java
+++ src/main/java/com/example/blaybus4th/global/config/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173","https://simvex-frontend-nu.vercel.app"));
         config.setAllowedMethods(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setExposedHeaders(List.of("Authorization"));
