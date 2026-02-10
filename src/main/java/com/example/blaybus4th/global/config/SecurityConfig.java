ffbf5bd114a744b95b56a8cc7ba9ccbfe0170306
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2026-02-10 12:21:34 +0900

    chore: CORS 설정 프론트엔드 url 추가

--- src/main/java/com/example/blaybus4th/global/config/SecurityConfig.java
+++ src/main/java/com/example/blaybus4th/global/config/SecurityConfig.java
@@ -45,7 +45,9 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://simvex-frontend-nu.vercel.app"));
         config.setAllowedMethods(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setExposedHeaders(List.of("Authorization"));
